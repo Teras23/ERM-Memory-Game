7b96717df481751cf4546aaba83c57581dbe7d7e
Karl Martin Teras <karlnitram@gmail.com>
2018-03-24 19:25:36 +0200

    Can change Activities

--- app/src/main/java/ee/erm/art/ermmemorygame/MainActivity.java
+++ app/src/main/java/ee/erm/art/ermmemorygame/MainActivity.java
@@ -1,21 +1,49 @@
 package ee.erm.art.ermmemorygame;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.logging.Logger;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private List<Question> questionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.question);
+        setContentView(R.layout.activity_main);
+
+        Button estonianButton = findViewById(R.id.languageEE);
+        Button russianButton = findViewById(R.id.languageRU);
+        Button englishButton = findViewById(R.id.languageEN);
+
+        estonianButton.setOnClickListener(this);
+        russianButton.setOnClickListener(this);
+        englishButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.languageEE) {
+            MLString.setCurrentLanguage(MLString.Language.Estonian);
+        }
+        else if(view.getId() == R.id.languageRU) {
+            MLString.setCurrentLanguage(MLString.Language.Russian);
+        }
+        else if(view.getId() == R.id.languageEN) {
+            MLString.setCurrentLanguage(MLString.Language.English);
+        }
+
+        startActivity(new Intent(MainActivity.this, QuestionActivity.class));
+        Log.i("ERM", "" + view.getId());
     }
 
     @Override
---
+++ app/src/main/java/ee/erm/art/ermmemorygame/QuestionActivity.java
@@ -0,0 +1,18 @@
+package ee.erm.art.ermmemorygame;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+/**
+ * Created by teras on 24.03.18.
+ */
+
+public class QuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.question);
+    }
+}
