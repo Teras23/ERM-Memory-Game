837cb03190ada8af00a7c31e314082a048f984b5
rannilo <33835169+rannilo@users.noreply.github.com>
2018-05-22 18:21:56 +0300

    added pictures, added correct/incorrect message, added margins

--- app/src/main/java/ee/erm/art/ermmemorygame/QuestionDescriptionActivity.java
+++ app/src/main/java/ee/erm/art/ermmemorygame/QuestionDescriptionActivity.java
@@ -3,13 +3,16 @@ package ee.erm.art.ermmemorygame;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import org.w3c.dom.Text;
 
+import java.io.File;
 import java.io.Serializable;
 import java.util.List;
 
@@ -31,16 +34,45 @@ public class QuestionDescriptionActivity extends AppCompatActivity implements Vi
         questionIndex = getIntent().getIntExtra("questionIndex", 0);
         Question question = questionList.get(questionIndex);
 
+        //setting correct/incorrect message
+        int selectedValue = getIntent().getIntExtra("answeredQuestion", 0);
+        TextView resultDescription = findViewById(R.id.resultDescription);
+        if(Integer.valueOf(selectedValue).equals(questionList.get(questionIndex).getCorrectAnswer())){
+            switch (MLString.getCurrentLanguage()){
+                case English:
+                    resultDescription.setText("Correct!");
+                    break;
+                case Russian:
+                    resultDescription.setText("Pravilno!");
+                    break;
+                case Estonian:
+                    resultDescription.setText("Õige!");
+                    break;
+            }
+        }
+        else{
+            switch (MLString.getCurrentLanguage()){
+                case Estonian:
+                    resultDescription.setText("Vale!");
+                    break;
+                case Russian:
+                    resultDescription.setText("Nepravilno otvet!");
+                    break;
+                case English:
+                    resultDescription.setText("Incorrect");
+                    break;
+            }
+        }
+
         TextView questionDescription = findViewById(R.id.questionDescription);
         questionDescription.setText(question.getDescription());
 
         ImageButton nextButton = findViewById(R.id.questionForward);
 
-
         ImageView imageView = findViewById(R.id.descriptionImage);
-        String filename = question.getFileName();
-        Bitmap bmImg = BitmapFactory.decodeFile(filename);
-        imageView.setImageBitmap(bmImg);
+       // Bitmap bmImg = BitmapFactory.decodeFile("p1.jpg");
+        imageView.setImageDrawable(getDrawable(getDrawableImage()));
+
 
         nextButton.setOnClickListener(this);
     }
@@ -65,21 +97,28 @@ public class QuestionDescriptionActivity extends AppCompatActivity implements Vi
             finish();
         }
     }
-
     private int getDrawableImage(){
         switch (questionIndex){
             case 0:
-                return R.drawable.pic1;
+                return R.drawable.p1;
             case 1:
-                return R.drawable.pic2;
+                return R.drawable.p2;
             case 2:
-                return R.drawable.pic3;
+                return R.drawable.p3;
             case 3:
-                return R.drawable.pic4;
+                return R.drawable.p4;
             case 4:
-                return R.drawable.pic5;
+                return R.drawable.p5;
             case 5:
-                return R.drawable.pic6;
+                return R.drawable.p6;
+            case 6:
+                return R.drawable.p7;
+            case 7:
+                return R.drawable.p8;
+            case 8:
+                return R.drawable.p9;
+            case 9:
+                return R.drawable.p10;
             default:
                 return R.drawable.ic_bg_pink;
         }
